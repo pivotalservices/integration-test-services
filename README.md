e9db829b085729cc1f1c1676fec3c64591714a12
John Calabrese <jcalabrese@pivotal.io>
2014-12-01 09:51:07 -0500

    adding wercker support
    adding required support for
    being used as a wercker step
    
    also adding readme file, for
    some explaination and guidance

---
+++ README.md
@@ -0,0 +1,33 @@
+# integration-test-services
+====================
+
+a utility to help with integration tests
+of bound services in cloud foundry.
+
+allows you to standup and destroy cf 
+services, pull binding information and
+destroy instances.
+
+### SAMPLE SCRIPTS: (sample_setup.sh & sample_teardown.sh)
+
+w/ support for wercker in the form of
+a wercker step to standup and destroy
+service instances and grab binding information
+
+
+### SAMPLE WERCKER USAGE:
+
+```
+deploy:
+  steps:
+    - xchapter7x/cf-bound-service-integration-test:
+        user_name: $cfuser
+        user_pass: $cfpass
+        org: $cforg
+        space: $cfspace
+        api_url: $cfurl
+        app_name: $cfappname
+        service_instance: $serviceinstance
+        service_type: $servicetype
+        action: (setup / cleanup)
+```
---
+++ run.sh
@@ -0,0 +1,77 @@
+#!/bin/sh
+if [ ! -n "$WERCKER_CF_INTEGRATION_TEST_SERVICE_API_URL" ]
+then
+    fail 'missing or empty option api_url, please check wercker.yml'
+fi
+
+if [ ! -n "$WERCKER_CF_INTEGRATION_TEST_SERVICE_USER_NAME" ]
+then
+    fail 'missing or empty option user_name, please check wercker.yml'
+fi
+
+if [ ! -n "$WERCKER_CF_INTEGRATION_TEST_SERVICE_USER_PASS" ]
+then
+    fail 'missing or empty option user_pass, please check wercker.yml'
+fi
+
+if [ ! -n "$WERCKER_CF_INTEGRATION_TEST_SERVICE_ORG" ]
+then
+    fail 'missing or empty option org, please check wercker.yml'
+fi
+
+if [ ! -n "$WERCKER_CF_INTEGRATION_TEST_SERVICE_SPACE" ]
+then
+    fail 'missing or empty option space, please check wercker.yml'
+fi
+
+if [ ! -n "$WERCKER_CF_INTEGRATION_TEST_SERVICE_APP_NAME" ]
+then
+    fail 'missing or empty option app_name, please check wercker.yml'
+fi
+
+if [ ! -n "$WERCKER_CF_INTEGRATION_TEST_SERVICE_SERVICE_INSTANCE_NAME" ]
+then
+    fail 'missing or empty option instance_name, please check wercker.yml'
+fi
+
+if [ ! -n "$WERCKER_CF_INTEGRATION_TEST_SERVICE_SERVICE_TYPE" ]
+then
+    fail 'missing or empty option service_type, please check wercker.yml'
+fi
+
+if [ ! -n "$WERCKER_CF_INTEGRATION_TEST_SERVICE_SERVICE_PLAN" ]
+then
+    fail 'missing or empty option service_plan, please check wercker.yml'
+fi
+
+if [ ! -n "$WERCKER_CF_INTEGRATION_TEST_SERVICE_ACTION" ]
+then
+    fail 'missing or empty option action, please check wercker.yml'
+fi
+
+
+wget http://go-cli.s3-website-us-east-1.amazonaws.com/releases/v6.3.2/cf-linux-amd64.tgz
+tar -zxvf cf-linux-amd64.tgz
+CF=$(pwd)/cf
+${CF} api ${WERCKER_CF_INTEGRATION_TEST_SERVICE_API_URL}
+
+${CF} login -u ${WERCKER_CF_INTEGRATION_TEST_SERVICE_USER_NAME} -p ${WERCKER_CF_INTEGRATION_TEST_SERVICE_USER_PASS} -o ${WERCKER_CF_INTEGRATION_TEST_SERVICE_ORG} -s ${WERCKER_CF_INTEGRATION_TEST_SERVICE_SPACE}
+
+export CF_CLI=${CF}
+export ORG=${WERCKER_CF_INTEGRATION_TEST_SERVICE_ORG}
+export SPACE=${WERCKER_CF_INTEGRATION_TEST_SERVICE_SPACE}
+export APP_NAME=${WERCKER_CF_INTEGRATION_TEST_SERVICE_APP_NAME}
+export SERVICE_INSTANCE_NAME=${WERCKER_CF_INTEGRATION_TEST_SERVICE_SERVICE_INSTANCE_NAME}
+export SERVICE_TYPE=${WERCKER_CF_INTEGRATION_TEST_SERVICE_SERVICE_TYPE}
+export SERVICE_PLAN=${WERCKER_CF_INTEGRATION_TEST_SERVICE_SERVICE_PLAN}
+export ACTION=${WERCKER_CF_INTEGRATION_TEST_SERVICE_ACTION}
+(cd $WERCKER_STEP_ROOT && python run.py)
+
+if [[ $? -ne 0 ]];then
+    warning $push_output
+    fail 'integration test helper run failed';
+
+else
+    success 'finished integration test helper run';
+
+fi
